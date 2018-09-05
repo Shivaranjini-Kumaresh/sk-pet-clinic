1e602de49cecff935fafef4820513e9f3c0b4b2c
Shivaranjini <s.arumugaperumal@walmartlabs.com>
2018-09-05 11:08:19 +0530

    Added owner page and controller. Closes #7

---
+++ sk-pet-clinic/petclinicweb/src/main/java/sk/springframework/skpetclinic/controllers/OwnerController.java
@@ -0,0 +1,14 @@
+package sk.springframework.skpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/owners")
+public class OwnerController {
+
+    @RequestMapping({"/","/ownerindex" , "/ownerindex.html"})
+    public String getOwners(){
+        return "owners/ownerindex";
+    }
+}
