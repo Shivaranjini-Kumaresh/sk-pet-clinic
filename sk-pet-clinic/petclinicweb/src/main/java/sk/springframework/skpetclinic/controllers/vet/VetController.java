12ebf392de5d492a17a1f4c9bc51e5a0a147c37e
Shivaranjini <s.arumugaperumal@walmartlabs.com>
2018-09-05 17:10:14 +0530

    Listing all vets Closes #9

--- sk-pet-clinic/petclinicweb/src/main/java/sk/springframework/skpetclinic/controllers/vet/VetController.java
+++ sk-pet-clinic/petclinicweb/src/main/java/sk/springframework/skpetclinic/controllers/vet/VetController.java
@@ -1,17 +1,23 @@
 package sk.springframework.skpetclinic.controllers.vet;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import sk.springframework.skpetclinic.services.impl.VetServiceMapImpl;
+import sk.springframework.skpetclinic.services.VetService;
 
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
 
     @RequestMapping({"/vets","vets/index","vets/vetindex.html"})
     public String getVets(Model model){
+        model.addAttribute("vets",vetService.findAll());
         return "vets/vetindex";
     }
 }
